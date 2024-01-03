4d365eb85d9a9e8def0729d33991fa141addf834
Andrés Weitzel <andres96energy@hotmail.com>
2024-01-03 18:40:49 -0300

    Added and checked tests

--- src/test/unit-test/api-integration/helpers/format/address.test.js
+++ src/test/unit-test/api-integration/helpers/format/address.test.js
@@ -56,57 +56,23 @@ describe("- splitAddressByLastDot helper (Unit Test)", () => {
       splitAddressByLastDotResult = await splitAddressByLastDot(undefined);
       await expect(typeof splitAddressByLastDotResult == "string").toBe(true);
     });
-
-    // msg = "Should return a string type if others arguments are passed ";
-    // it(msg, async () => {
-    //   splitAddressByLastDotResult = await currentDateTime(TODAY_DATE);
-    //   await expect(typeof splitAddressByLastDotResult == "string").toBe(true);
-    // });
-
-    // msg = "Should return a string type if a null value is passed";
-    // it(msg, async () => {
-    //   splitAddressByLastDotResult = await currentDateTime(null);
-    //   await expect(typeof splitAddressByLastDotResult == "string").toBe(true);
-    // });
-
-    // msg = "Should return a string type if a undefined value is passed";
-    // it(msg, async () => {
-    //   splitAddressByLastDotResult = await currentDateTime(undefined);
-    //   await expect(typeof splitAddressByLastDotResult == "string").toBe(true);
-    // });
   });
 
-  //   describe("2) Check cases for return.", () => {
-  //     msg =
-  //       "Should return a string type with 'DD/MM/YY HH:MM:SS' format (Ex: 2023-03-18 21:06:15)";
-  //     it(msg, async () => {
-  //       splitAddressByLastDotResult = await currentDateTime();
-  //       await expect(typeof splitAddressByLastDotResult == "string").toBe(true);
-  //       let characters = "/,:";
-  //       let totalCharacters = 4; // total characters for DD/MM/YY HH:MM:SS
-  //       let numberCharactersMatch = await calculateNumberOfCharactersMatch(
-  //         splitAddressByLastDotResult,
-  //         characters
-  //       );
-  //       await expect(numberCharactersMatch >= totalCharacters).toBe(true);
-  //     });
-  //   });
+  describe("2) Check cases for error.", () => {
+    msg =
+      "Should not return a error message and not throw an error if no argument is passed to the function.";
+    it(msg, async () => {
+      await expect(async () => await splitAddressByLastDot()).not.toThrow(
+        Error
+      );
+    });
 
-  //   describe("3) Check cases for error.", () => {
-  //     msg =
-  //       "Should not return a error message if no argument is passed to the function.";
-  //     it(msg, async () => {
-  //       await expect(async () => await currentDateTime()).not.toThrow(Error);
-  //       splitAddressByLastDotResult = await currentDateTime();
-  //       await expect(typeof splitAddressByLastDotResult == "string").toBe(true);
-  //     });
-
-  //     msg =
-  //       "Should return a string and not throw an Error if a new Error is passed";
-  //     it(msg, async () => {
-  //       splitAddressByLastDotResult = await currentDateTime(new Error());
-  //       await expect(async () => splitAddressByLastDotResult).not.toThrow(Error);
-  //       await expect(typeof splitAddressByLastDotResult == "string").toBe(true);
-  //     });
-  //   });
+    msg =
+      "Should not return a error message and not throw an error if a new Error() is passed to the function.";
+    it(msg, async () => {
+      await expect(
+        async () => await splitAddressByLastDot(new Error())
+      ).not.toThrow(Error);
+    });
+  });
 });
