41dabdd47c05fdf6a67a3bb04b4c23f2906b43ff
Andrés Weitzel <andres96energy@hotmail.com>
2024-01-09 00:39:39 -0300

    Updated readme

--- src/test/unit-test/api-integration/helpers/request/get-data-from-address.test.js
+++ src/test/unit-test/api-integration/helpers/request/get-data-from-address.test.js
@@ -54,46 +54,40 @@ describe("- getDataFromSpecificAddress helper (Unit Test)", () => {
         true
       );
     });
-    // msg =
-    //   "Should return a object type if a valid argument is passed (This function expects a single argument)";
-    // it(msg, async () => {
-    //   getDataFromSpecificAddressResult = await getDataFromSpecificAddress(
-    //     MOCK_STRING_VALUE
-    //   );
-    //   await expect(typeof getDataFromSpecificAddressResult == "object").toBe(true);
-    // });
 
-    // msg =
-    //   "Should return a object type if two valid arguments are passed (This function expects a single argument)";
-    // it(msg, async () => {
-    //   getDataFromSpecificAddressResult = await getDataFromSpecificAddress(
-    //     MOCK_STRING_VALUE,
-    //     MOCK_STRING_VALUE
-    //   );
-    //   await expect(typeof getDataFromSpecificAddressResult == "object").toBe(true);
-    // });
+    msg =
+      "Should return a string type if a null value is passed (This function expects a single argument of string type)";
+    it(msg, async () => {
+      getDataFromSpecificAddressResult = await getDataFromSpecificAddress(null);
+      await expect(typeof getDataFromSpecificAddressResult == "string").toBe(
+        true
+      );
+    });
 
-    // msg =
-    //   "Should return a string type if an invalid argument is passed (This function expects a single argument)";
-    // it(msg, async () => {
-    //   getDataFromSpecificAddressResult = await getDataFromSpecificAddress(122212131);
-    //   await expect(typeof getDataFromSpecificAddressResult == "string").toBe(true);
-    // });
-
-    // msg =
-    //   "Should return a string type if a null value is passed (This function expects a single argument)";
-    // it(msg, async () => {
-    //   getDataFromSpecificAddressResult = await getDataFromSpecificAddress(null);
-    //   await expect(typeof getDataFromSpecificAddressResult == "string").toBe(true);
-    // });
-
-    // msg =
-    //   "Should return a string type if a undefined value is passed (This function expects a single argument)";
-    // it(msg, async () => {
-    //   getDataFromSpecificAddressResult = await getDataFromSpecificAddress(undefined);
-    //   await expect(typeof getDataFromSpecificAddressResult == "string").toBe(true);
-    // });
+    msg =
+      "Should return a string type if an undefined value is passed (This function expects a single argument of string type)";
+    it(msg, async () => {
+      getDataFromSpecificAddressResult = await getDataFromSpecificAddress(
+        undefined
+      );
+      await expect(typeof getDataFromSpecificAddressResult == "string").toBe(
+        true
+      );
+    });
   });
+
+  // describe("2) Check cases return cases.", () => {
+  //   msg =
+  //     "Should return a object type with data attribute if a valid ip is passed (This function expects a single argument of string type)";
+  //   it(msg, async () => {
+  //     getDataFromSpecificAddressResult = await getDataFromSpecificAddress(
+  //       MOCK_VALID_IP_VALUE
+  //     );
+  //     // await expect(typeof getDataFromSpecificAddressResult == "string").toBe(
+  //     //   true
+  //     // );
+  //   });
+  // });
 
   //   describe("2) Check cases for error.", () => {
   //     msg =
