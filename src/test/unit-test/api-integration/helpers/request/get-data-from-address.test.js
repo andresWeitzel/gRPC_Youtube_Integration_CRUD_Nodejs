74d84fc9bf8074fb13ed8fbe77d9aaf008fb5fdf
Andrés Weitzel <andres96energy@hotmail.com>
2024-01-24 22:14:05 -0300

    Checked bug jest config

--- src/test/unit-test/api-integration/helpers/request/get-data-from-address.test.js
+++ src/test/unit-test/api-integration/helpers/request/get-data-from-address.test.js
@@ -4,7 +4,7 @@ const {
   getDataFromSpecificAddress,
 } = require("../../../../../api-integration/helpers/request/get-data-from-address");
 //Const
-const MOCK_OBJECT_KEY_01 = process.env.MOCK_OBJECT_KEY_01;
+const MOCK_OBJECT_KEY_01 = process.env.MOCK_OBJECT_VALUE_01;
 const MOCK_OBJECT_01 = { mock_object_key: MOCK_OBJECT_KEY_01 };
 const MOCK_INVALID_IP_VALUE_01 = process.env.MOCK_INVALID_IP_01;
 const MOCK_VALID_IP_VALUE_01 = process.env.MOCK_VALID_IP_01;
@@ -15,32 +15,32 @@ let getDataFromSpecificAddressResult;
 describe("- getDataFromSpecificAddress helper (Unit Test)", () => {
   describe("1) Check cases for arguments.", () => {
     msg =
-      "Should return a object type if no arguments are passed (This function expects a single argument of string type)";
+      "Should return a string type if no arguments are passed (This function expects a single argument of string type)";
     it(msg, async () => {
       getDataFromSpecificAddressResult = await getDataFromSpecificAddress();
-      await expect(typeof getDataFromSpecificAddressResult == "object").toBe(
+      await expect(typeof getDataFromSpecificAddressResult == "string").toBe(
         true
       );
     });
 
     msg =
-      "Should return a object type if not string argument is passed (This function expects a single argument of string type)";
+      "Should return a string type if not string argument is passed (This function expects a single argument of string type)";
     it(msg, async () => {
       getDataFromSpecificAddressResult = await getDataFromSpecificAddress(
         MOCK_OBJECT_01
       );
-      await expect(typeof getDataFromSpecificAddressResult == "object").toBe(
+      await expect(typeof getDataFromSpecificAddressResult == "string").toBe(
         true
       );
     });
 
     msg =
-      "Should return a object type if an invalid ip is passed (This function expects a single argument of string type)";
+      "Should return a string type if an invalid ip is passed (This function expects a single argument of string type)";
     it(msg, async () => {
       getDataFromSpecificAddressResult = await getDataFromSpecificAddress(
         MOCK_INVALID_IP_VALUE_01
       );
-      await expect(typeof getDataFromSpecificAddressResult == "object").toBe(
+      await expect(typeof getDataFromSpecificAddressResult == "string").toBe(
         true
       );
     });
@@ -51,27 +51,29 @@ describe("- getDataFromSpecificAddress helper (Unit Test)", () => {
       getDataFromSpecificAddressResult = await getDataFromSpecificAddress(
         MOCK_VALID_IP_VALUE_01
       );
+      
+      console.log("SS656756565655"+getDataFromSpecificAddressResult);
       await expect(typeof getDataFromSpecificAddressResult == "object").toBe(
         true
       );
     });
 
     msg =
-      "Should return a object type if a null value is passed (This function expects a single argument of string type)";
+      "Should return a string type if a null value is passed (This function expects a single argument of string type)";
     it(msg, async () => {
       getDataFromSpecificAddressResult = await getDataFromSpecificAddress(null);
-      await expect(typeof getDataFromSpecificAddressResult == "object").toBe(
+      await expect(typeof getDataFromSpecificAddressResult == "string").toBe(
         true
       );
     });
 
     msg =
-      "Should return a object type if an undefined value is passed (This function expects a single argument of string type)";
+      "Should return a string type if an undefined value is passed (This function expects a single argument of string type)";
     it(msg, async () => {
       getDataFromSpecificAddressResult = await getDataFromSpecificAddress(
         undefined
       );
-      await expect(typeof getDataFromSpecificAddressResult == "object").toBe(
+      await expect(typeof getDataFromSpecificAddressResult == "string").toBe(
         true
       );
     });
